ff10fdc684cfb13fb251a40d62061db4665a4c4e
Marcus Schäfer <ms@suse.de>
2015-05-05 23:23:40 +0200

    Added DataOutput class
    In order to support different output styles e.g json, XML or machine
    readable a new class to encapsulate this has been introduced. By
    default we will use json as output format. Along with this change
    also colored output is added which Fixes #11

--- test/unit/data_collector_test.py
+++ test/unit/data_collector_test.py
@@ -9,7 +9,10 @@ from azure_cli.data_collector import DataCollector
 class TestDataCollector:
     def setup(self):
         self.collector = DataCollector()
-        self.collector.add('some-name', 'some-data')
 
     def test_get(self):
+        assert self.collector.get() == {}
+
+    def test_add(self):
+        self.collector.add('some-name', 'some-data')
         assert self.collector.get() == {'some-name': 'some-data'}
---
+++ test/unit/data_output_test.py
@@ -0,0 +1,24 @@
+import sys
+from mock import patch
+from nose.tools import *
+
+import nose_helper
+
+from azure_cli.data_output import DataOutput
+from azure_cli.data_collector import DataCollector
+import json
+
+
+class TestDataOutput:
+    def setup(self):
+        collector = DataCollector()
+        collector.add('some-name', 'some-data')
+        self.expected_out = json.dumps(
+            collector.get(), sort_keys=True, indent=2, separators=(',', ': ')
+        )
+        self.out = DataOutput(collector)
+
+    @patch('sys.stdout')
+    def test_display(self, mock_stdout):
+        self.out.display()
+        mock_stdout.write.assert_any_call(self.expected_out)
